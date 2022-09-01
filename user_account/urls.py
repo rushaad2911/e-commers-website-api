85cefaa2fd8be39f7fbbd44f7b065033927f8fdf
rushaad <m.rushaadq@gmail.com>
2022-09-01 14:40:48 +0530

    added allauth url

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from .views import CreateUser
 
 urlpatterns = [
     
     path('create/',CreateUser.as_view(),name='user'),
+    path('',include('allauth.urls')),
     
 ]
